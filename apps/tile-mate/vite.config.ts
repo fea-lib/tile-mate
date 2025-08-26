082a1d4eb6f1dec69161547e70c973085008c689
Tobias Belch <tobias.belch@docmorris.de>
2025-08-26 08:26:40 +0200

    chore: add base url for prod

--- apps/tile-mate/vite.config.ts
+++ apps/tile-mate/vite.config.ts
@@ -1,13 +1,14 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/tile-mate/" : "/",
   plugins: [solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-});
+}));
