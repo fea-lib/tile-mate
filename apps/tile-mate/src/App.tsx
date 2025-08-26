c0445a51a0e6fa0994390a08c83e9b6b167488c8
Tobias Belch <tobias.belch@docmorris.de>
2025-08-26 09:44:17 +0200

    feat: render a tileset

--- apps/tile-mate/src/App.tsx
+++ apps/tile-mate/src/App.tsx
@@ -1,9 +1,9 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import Tileset from "./Tileset";
+import tilesetImage from "./tileset.png";
 
 const App: Component = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
+  return <Tileset showGrid tilesetImage={tilesetImage} />;
 };
 
 export default App;
--- apps/tile-mate/vite.config.ts
+++ apps/tile-mate/vite.config.ts
@@ -1,10 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/tile-mate/" : "/",
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
