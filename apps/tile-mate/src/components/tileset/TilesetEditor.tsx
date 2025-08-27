49285095d86b3f3ebb6271e98bb2eaa927891742
Tobias Belch <tobias.belch@docmorris.de>
2025-08-27 14:17:55 +0200

    feat: start building out the editor

--- apps/tile-mate/src/components/tileset/TilesetEditor.tsx
+++ apps/tile-mate/src/components/tileset/TilesetEditor.tsx
@@ -1,6 +1,7 @@
 import type { Component } from "solid-js";
 import { TilesetContextProvider } from "./TilesetContext";
 import { Tileset } from "./Tileset";
+import staticStyles from "./TilesetEditor.module.css";
 
 type Props = {
   tilesetImage: string;
@@ -15,7 +16,21 @@ export const TilesetEditor: Component<Props> = ({
 }) => {
   return (
     <TilesetContextProvider tileSize={tileSize} tilesetImage={tilesetImage}>
-      <Tileset showGrid={showGrid} />
+      <div class={staticStyles.tilesetEditor}>
+        <Actions />
+        <div class={staticStyles.tileset}>
+          <Tileset showGrid={showGrid} />
+        </div>
+      </div>
     </TilesetContextProvider>
+  );
+};
+
+const Actions: Component = () => {
+  return (
+    <div class={staticStyles.actions}>
+      <button>Save</button>
+      <button>Cancel</button>
+    </div>
   );
 };
