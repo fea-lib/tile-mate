a51cab3503e67a59ebf4847ba255ae77889f1f21
Tobias Belch <tobias.belch@docmorris.de>
2025-09-02 15:19:33 +0200

    chore: remove demo tileset

--- apps/tile-mate/src/App.tsx
+++ apps/tile-mate/src/App.tsx
@@ -1,22 +1,10 @@
-import { onMount, type Component } from "solid-js";
-import tilesetImage from "./tileset.png";
+import { type Component } from "solid-js";
 import { TilesetEditor } from "./tileset-editor/TilesetEditor";
 import { useTileMateStore } from "./store/TileMateStore";
-
-const tileSize = 48;
 
 const App: Component = () => {
   const { addTileset, tilesets } = useTileMateStore();
 
-  onMount(async () => {
-    if (tilesets().length > 0) return;
-
-    try {
-      await Promise.all([addTileset(tilesetImage, tileSize)]);
-    } catch (error) {
-      console.error("Failed to load tileset:", error);
-    }
-  });
   return <TilesetEditor />;
 };
 
